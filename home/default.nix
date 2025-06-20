855c26479c4d6a9e020fcf8dbcb970ccabb3523c
NalonOff <103069812+NalonOff@users.noreply.github.com>
2025-06-20 23:34:07 +0200

    Update default.nix
    Add session variables for a better wayland support

--- home/default.nix
+++ home/default.nix
@@ -49,6 +49,22 @@
   
   # Environment variables
   home.sessionVariables = {
+    
+    # Electron / Ozone
+    NIXOS_OZONE_WL = "1";
+    ELECTRON_OZONE_PLATFORM_HINT = "auto";
+    OZONE_PLATFORM = "wayland";
+
+    # Qt / GTK scaling & Wayland support
+    GDK_SCALE = "1";
+    GDK_DPI_SCALE = "1";
+    GTK_USE_PORTAL = "1";
+    QT_QPA_PLATFORM = "wayland";
+    QT_WAYLAND_DISABLE_WINDOWDECORATION = "1";
+
+    # Session & compatibility
+    XDG_SESSION_TYPE = "wayland";
+
     EDITOR = "neovim";
     PAGER = "less";
   };
