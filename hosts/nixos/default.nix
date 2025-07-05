b158fb7c52726d6da8c9f8555a982a54d82bdce0
Nalon <nalonoff@gmail.com>
2025-07-05 16:47:22 +0200

    Add audio

--- hosts/nixos/default.nix
+++ hosts/nixos/default.nix
@@ -9,21 +9,22 @@
     ../../modules/system
     ../../modules/fonts
     ../../modules/ssh
-  ]; 
+    ../../modules/audio
+  ];
 
   # Version du système
   system.stateVersion = "25.05";
-  
+
   # Nom d'hôte
   networking.hostName = "nixos";
-  
+
   # Fuseau horaire
   time.timeZone = "Europe/Paris";
-  
+
   # Localisation
   i18n.defaultLocale = "fr_FR.UTF-8";
   console.keyMap = "fr";
-  
+
   # Flakes et commande nix
   nix = {
     settings = {
@@ -40,7 +41,7 @@
 
   boot.kernelParams = [ "rtw_8821ce.fwctrl_lps=0" "rtw_8821ce.swlps=0" ];
   hardware.enableAllFirmware = true;
-  
+
   # Logiciels propriétaires
   nixpkgs.config.allowUnfree = true;
 }
