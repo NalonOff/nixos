d2f297c3005f6df44a354b349e73c56dbd5c7acf
Nalon <nalonoff@gmail.com>
2025-12-03 17:29:24 +0100

    Switch to Xantusia and change nixvim input to fix the LSP issue (ansible, which is no longer supported)

--- flake.nix
+++ flake.nix
@@ -2,10 +2,10 @@
   description = "NixOS configuration flake";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
 
     home-manager = {
-      url = "github:nix-community/home-manager/release-25.05";
+      url = "github:nix-community/home-manager/release-25.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
@@ -20,7 +20,7 @@
     };
 
     nixvim = {
-      url = "github:nix-community/nixvim/nixos-25.05";
+      url = "github:jfredett/nixvim";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
