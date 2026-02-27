08e72bfb183dc51720c264318ced9b56d689ff5b
NalonOff <nalonoff@gmail.com>
2026-02-28 00:52:17 +0100

    Rewrite aliases for system managing

--- home/programs/zsh.nix
+++ home/programs/zsh.nix
@@ -6,24 +6,24 @@
     enableCompletion = true;
     autosuggestion.enable = true;
     syntaxHighlighting.enable = true;
-    
+
     history = {
       size = 10000;
       path = "${config.xdg.dataHome}/zsh/history";
     };
-    
+
     shellAliases = {
       ll = "ls -l";
       la = "ls -la";
       ".." = "cd ..";
       "..." = "cd ../..";
       grep = "grep --color=auto";
-      
+
       # NixOS specificatons
-      rebuild = "sudo nixos-rebuild switch --flake /home/user/dotfiles";
-      update = "cd /home/user/dotfiles && nix flake update && sudo nixos-rebuild switch --flake .";
+      update = ''f() { cd "$HOME/dotfiles" && nix flake update && sudo nixos-rebuild switch --flake ".#$1"; }; f'';
+      rbs = ''f() { sudo nixos-rebuild switch --flake "$HOME/dotfiles/.#$1"; }; f'';
     };
-    
+
     # Prompt configuration
     initContent = ''pfetch; eval "$(starship init zsh)"'';
    };
